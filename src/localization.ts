3412ea06c007a37df6a802d67b278388adb99275
DmitryUniversall <dmitrii.murashov@gmail.com>
2025-01-21 01:08:59 +0300

    Localization initialization fix; EN localization

--- src/localization.ts
+++ src/localization.ts
@@ -1,19 +1,40 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend'
+
+// Localization RU
+import commonLocalizationRU from "./locales/ru/common.json"
+import footerLocalizationRU from "./locales/ru/footer.json"
+import indexLocalizationRU from "./locales/ru/index.json"
+import menuLocalizationRU from "./locales/ru/menu.json"
+
+// Localization EN
+import commonLocalizationEN from "./locales/en/common.json"
+import footerLocalizationEN from "./locales/en/footer.json"
+import indexLocalizationEN from "./locales/en/index.json"
+import menuLocalizationEN from "./locales/en/menu.json"
+
 
 export function initializeLocalization(): void {
-    i18n
-        .use(Backend)
-        .use(initReactI18next)
+    i18n.use(initReactI18next)
         .init({
             debug: true,
             lng: 'ru',
             fallbackLng: 'ru',
             interpolation: { escapeValue: false },
             defaultNS: 'common',
-            backend: {
-                loadPath: '/src/locales/{{lng}}/{{ns}}.json',
-            },
+            resources: {
+                ru: {
+                    common: commonLocalizationRU,
+                    footer: footerLocalizationRU,
+                    index: indexLocalizationRU,
+                    menu: menuLocalizationRU
+                },
+                en: {
+                    common: commonLocalizationEN,
+                    footer: footerLocalizationEN,
+                    index: indexLocalizationEN,
+                    menu: menuLocalizationEN
+                }
+            }
         });
 }
